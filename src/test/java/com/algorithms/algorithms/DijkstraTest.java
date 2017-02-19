fc241db218818e2ce0b963270072d15b2f5d4d05
Ayomitunde Fafore <Ayomitunde Fafore>
2017-02-18 21:15:36 -0500

    refactored and adding more test

---
+++ src/test/java/com/algorithms/algorithms/DijkstraTest.java
@@ -0,0 +1,16 @@
+package com.algorithms.algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ayomitunde on 2/18/2017.
+ */
+public class DijkstraTest {
+    @Test
+    public void run() throws Exception {
+
+    }
+
+}
